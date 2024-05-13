4821537f628cd4a5f71d1917a462f38e31ad5579
Owen Blacker <owen@blacker.me.uk>
2024-05-13 14:55:04 +0100

    Equality testing

--- test-class.js
+++ test-class.js
@@ -1,7 +1,11 @@
-class TestClass{
-    aMethod(){
-        return true;
+class Probability {
+    equals(comparator) {
+        return this.likelihood === comparator.likelihood;
+    }
+
+    Probability(probability) {
+        this.likelihood = probability;
     }
 }
 
-module.exports = {TestClass}
+module.exports = {Probability}
--- test-class.test.js
+++ test-class.test.js
@@ -1,7 +1,16 @@
-const {TestClass} = require("./test-class")
+const {Probability} = require("./test-class")
 
-describe('TestClass...', () => {
-    test('', () => {
-        expect(new TestClass().aMethod()).toBe(true);
+describe('Test instances', () => {
+    test('Are instances equal', () => {
+        let p1 = new Probability();
+
+        expect(p1.equals(p1)).toBe(true);
+    })
+
+    test('Are different instances unequal', () => {
+        let p1 = new Probability();
+        let p2 = new Probability();
+
+        expect(p1.equals(p2)).toBe(false);
     })
 })
